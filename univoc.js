7ddc24b8963810379652be8022a0e52622888ba6
vrinda07-ui <banothvrinda@gmail.com>
2024-12-22 02:14:10 +0530

    Initial commit

---
+++ univoc.js
@@ -0,0 +1,16 @@
+document.getElementById('start-learning').addEventListener('click', () => {
+    alert('Thank you for your interest! Your learning journey is about to begin.');
+  });
+  
+
+
+document.querySelector('.start-learning').addEventListener('click', () => {
+    alert('Redirecting to the course page...');
+});
+
+
+
+
+document.querySelector('.submit').addEventListener('click', () => {
+    alert('Redirecting to the course page...');
+});
